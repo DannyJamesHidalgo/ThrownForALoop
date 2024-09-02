c57c28140ad1d3200518bd05b135ad1f1b1a0d1b
Danny Hidalgo <dnnysongs@gmail.com>
2024-09-02 15:38:06 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,191 @@
+﻿
+
+
+List<Product> products = new List<Product>()
+{
+    new Product()
+    { 
+        Name = "Football", 
+        Price = 15.00M, 
+        Sold = false,
+        StockDate = new DateTime(2024, 08, 20),
+        ManufactureYear = 2010,
+        Condition = 4.2
+    },
+    new() 
+    { 
+        Name = "Hockey Stick", 
+        Price = 12.00M, 
+        Sold = false,
+        StockDate = new DateTime(2023,11,20),
+        ManufactureYear = 2011,
+        Condition = 5.6
+        
+    },
+    new()
+    {
+        Name ="Golf Club",
+        Price = 35.00M,
+        Sold = false,
+        StockDate =new DateTime(2023,10,25),
+        ManufactureYear= 2015,
+          Condition = 5.2
+    },
+    new()
+    {
+        Name ="Baseball",
+        Price = 10.00M,
+        Sold = false,
+        StockDate =new DateTime(2023,10,25),
+        ManufactureYear= 2015,
+        Condition = 6.6
+        
+    },
+    new Product()
+    {
+        Name ="Glove",
+        Price = 40.00M,
+        Sold = false,
+        StockDate =new DateTime(2023,10,25),
+        ManufactureYear= 2015,
+        Condition = 5.5
+        
+    },
+    new Product()
+    {
+        Name ="Boots",
+        Price = 35.00M,
+        Sold = false,
+        StockDate =new DateTime(2023,10,25),
+        ManufactureYear= 2015,
+        Condition = 6.8
+    }
+};
+
+// List<string> products = new List<string>()
+// {
+//     "Football",
+//     "Hockey Stick",
+//     "Boomerang",
+//     "Frisbee",
+//     "Golf Putter"
+// };
+string greeting = @"Welcome to Thrown For a Loop
+Your one-stop shop for used sporting equipment";
+Console.WriteLine(greeting);
+
+
+string choice = null;
+while (choice != "0")
+{
+    Console.WriteLine(@"Choose an option:
+                        0. Exit
+                        1. View All Products
+                        2. View Product Details
+                        3. View Latest Products");
+    choice = Console.ReadLine();
+    if (choice == "0")
+    {
+        Console.WriteLine("Goodbye!");
+    }
+    else if (choice == "1")
+    {
+        ListProducts();
+    }
+    else if (choice == "2")
+    {
+        ViewProductDetails();
+    }
+    else if (choice == "3")
+    {
+        ViewLatestProducts();
+    }
+    
+}
+
+
+
+
+
+
+
+
+void ListProducts()
+{
+    decimal totalValue = 0.0M;
+    foreach (Product product in products)
+    {
+        if (!product.Sold)
+        {
+            totalValue += product.Price;
+        }
+    }
+    Console.WriteLine($"Total inventory value: ${totalValue}");
+    Console.WriteLine("Products:");
+    for (int i = 0; i < products.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {products[i].Name}");
+    }
+}
+
+
+void ViewProductDetails()
+{
+    ListProducts();
+
+Product chosenProduct = null;
+
+while (chosenProduct == null)
+{
+    Console.WriteLine("Please enter a product number: ");
+    try
+{
+   int response = int.Parse(Console.ReadLine().Trim());
+   chosenProduct = products[response - 1];
+}
+catch (FormatException)
+{
+   Console.WriteLine("Please type only integers!");
+}
+catch (ArgumentOutOfRangeException)
+{
+   Console.WriteLine("Please choose an existing item only!");
+}
+catch (Exception ex)
+{
+   Console.WriteLine(ex);
+   Console.WriteLine("Do Better!");
+}
+}
+
+DateTime now = DateTime.Now;
+
+TimeSpan timeInStock = now - chosenProduct.StockDate;
+Console.WriteLine(@$"You chose: 
+{chosenProduct.Name}, which costs {chosenProduct.Price} dollars.
+It is {now.Year - chosenProduct.ManufactureYear} years old. 
+It {(chosenProduct.Sold ? "is not available." : $"has been in stock for {timeInStock.Days} days.")}");
+
+}
+
+void ViewLatestProducts()
+{
+    // create a new empty List to store the latest products
+    List<Product> latestProducts = new List<Product>();
+    // Calculate a DateTime 90 days in the past
+    DateTime threeMonthsAgo = DateTime.Now - TimeSpan.FromDays(90);
+    //loop through the products
+    foreach (Product product in products)
+    {
+        //Add a product to latestProducts if it fits the criteria
+        if (product.StockDate > threeMonthsAgo && !product.Sold)
+        {
+            latestProducts.Add(product);
+        }
+    }
+    // print out the latest products to the console 
+    for (int i = 0; i < latestProducts.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {latestProducts[i].Name}");
+    }
+}
